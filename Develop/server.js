da03744951651494b3f7eaba8c59313812a0de27
Brandon Bayer <brandonabayer@gmail.com>
2020-04-21 15:28:58 -0500

    initial commit with readme and repo

---
+++ Develop/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "",
+    {
+        useNewUrlParser: true,
+    }
+);
+
+// Requiring our routes
+require("./routes/api-routes.js.js.js.js")(app);
+require("./routes/html-routes.js.js.js.js")(app);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
